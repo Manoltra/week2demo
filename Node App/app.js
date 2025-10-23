2b4c124761b6c554112e639ad75703bbc71c6947
Traian Manolescu <tpm@micro-workflow.com>
2025-10-23 13:52:43 -0700

    Is this commiting?

--- Node App/app.js
+++ Node App/app.js
@@ -1,34 +1,41 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
 
-// Middleware example
+// Load environment variables
+dotenv.config();
+
+// Middleware for logging requests
 app.use((req, res, next) => {
     console.log(`${req.method} request for '${req.url}'`);
     next();
 });
 
-// Default page
+// Routes
 app.get('/', (req, res) => {
     res.send('Welcome to the Default Page!');
 });
 
-// About page
 app.get('/about', (req, res) => {
     res.send('This is the About Page.');
 });
 
-// Contact page
 app.get('/contact', (req, res) => {
     res.send('This is the Contact Page.');
 });
 
-// POST request handler
 app.post('/submit', (req, res) => {
     res.send('Post request received!');
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
